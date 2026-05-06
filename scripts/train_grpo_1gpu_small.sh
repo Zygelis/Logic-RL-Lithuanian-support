1eecfa64e3ab74a7f641686ec0cb9df3d1fb881b
Zygimantas Girdauskas <zygimantas@aai-labs.com>
2026-05-06 21:53:04 +0300

    mini change to test script

--- scripts/train_grpo_1gpu_small.sh
+++ scripts/train_grpo_1gpu_small.sh
@@ -12,6 +12,10 @@ ARGS+=" data.val_batch_size=1"
 ARGS+=" data.max_prompt_length=128"
 ARGS+=" data.max_response_length=256"
 ARGS+=" actor_rollout_ref.model.path=Qwen/Qwen2.5-1.5B-Instruct"
+ARGS+=" +actor_rollout_ref.model.override_config.torch_dtype=bfloat16"
+ARGS+=" +actor_rollout_ref.model.override_config.device_map=auto"
+ARGS+=" +actor_rollout_ref.model.override_config.low_cpu_mem_usage=true"
+ARGS+=" +actor_rollout_ref.model.override_config.trust_remote_code=true"
 ARGS+=" actor_rollout_ref.actor.optim.lr=3e-07"
 ARGS+=" actor_rollout_ref.model.use_remove_padding=True"
 ARGS+=" actor_rollout_ref.actor.ppo_mini_batch_size=1"
@@ -25,13 +29,13 @@ ARGS+=" ++actor_rollout_ref.actor.fsdp_config.param_offload=true"
 ARGS+=" ++actor_rollout_ref.actor.fsdp_config.grad_offload=true"
 ARGS+=" ++actor_rollout_ref.actor.fsdp_config.optimizer_offload=true"
 
-# Use vLLM rollout but keep resource usage low
+# Use vLLM rollout on 2 GPUs
 ARGS+=" actor_rollout_ref.rollout.name=vllm"
-ARGS+=" actor_rollout_ref.rollout.gpu_memory_utilization=0.5"
+ARGS+=" actor_rollout_ref.rollout.gpu_memory_utilization=0.6"
+ARGS+=" actor_rollout_ref.rollout.tensor_model_parallel_size=2"
 ARGS+=" actor_rollout_ref.rollout.n=1"
 ARGS+=" actor_rollout_ref.rollout.prompt_length=128"
 ARGS+=" actor_rollout_ref.rollout.response_length=128"
-ARGS+=" actor_rollout_ref.rollout.tensor_model_parallel_size=1"
 
 # Reference model offloaded
 ARGS+=" ++actor_rollout_ref.ref.fsdp_config.param_offload=true"
@@ -43,7 +47,7 @@ ARGS+=" trainer.save_freq=-1"
 ARGS+=" trainer.logger=['console']"
 ARGS+=" trainer.project_name='grpo_kk_small_test'"
 ARGS+=" trainer.experiment_name='grpo_qwen2_small'" 
-ARGS+=" trainer.n_gpus_per_node=1"
+ARGS+=" trainer.n_gpus_per_node=2"
 ARGS+=" trainer.nnodes=1"
 
 python3 -u -m verl.trainer.main_ppo $ARGS
